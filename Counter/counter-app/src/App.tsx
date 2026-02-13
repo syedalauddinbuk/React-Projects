353a47c00d5c5e44d5b68d18cfc84f70435db5ef
Syed Alauddin Bukhari <syedalauddin.b@gmail.com>
2026-02-12 22:15:50 -0700

    update some comment

--- Counter/counter-app/src/App.tsx
+++ Counter/counter-app/src/App.tsx
@@ -2,7 +2,7 @@ import { useState } from 'react'
 import reactLogo from './assets/react.svg'
 import viteLogo from '/vite.svg'
 import './App.css'
-import {Counter} from './components/Counter'
+import {Counter} from './components/Counter' //taken the counter from counter component
 
 
 function App() {
