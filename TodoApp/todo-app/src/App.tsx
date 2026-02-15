4b1bf66be4c7f93ddae169bd247760bb25874255
Syed Alauddin Bukhari <syedalauddin.b@gmail.com>
2026-02-15 12:57:24 -0700

    refactor: restructure Todo app components and implement core functionality

--- TodoApp/todo-app/src/App.tsx
+++ TodoApp/todo-app/src/App.tsx
@@ -1,21 +1,54 @@
-import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import viteLogo from '/vite.svg'
-import './App.css'
+import {useState} from "react";
+import {Todo} from "./types/todo";
+import TodoInput from "./components/TodoInput";
+import TodoList from "./components/TodoList";
+
+
 
 function App() {
-  
 
+  const [todos, setTodos] = useState<Todo[]>([]);
+
+  const addTodo = (text:string)=>{
+    const newTodo: Todo = {
+id:crypto.randomUUID(),
+text,
+completed: false,
+createdAt: new Date(),
+
+
+    };
+    setTodos(prev=>[...prev,newTodo]);
+
+  }
+  
+const toggleTodo=(id:string)=>{
+  setTodos(prev=>
+    prev.map(todo=>
+      todo.id=== id?{...todo,completed:!todo.completed}:todo
+    )
+  );
+
+const deleteTodo = (id:string)=>{
+  setTodos(prev=>prev.filter(todo=>todo.id!=id));
+};
+ 
+
+}
   return (
-    <>
-   //Header
+    <div style={{maxWidth: 500, margin: "auto"}}>
+      <h1>Todo App</h1>
+      <TodoInput onAdd ={addTodo} />
+      <TodoList
+      todos={todos}
+      onToggle={toggleTodo}
+      onDelete={deleteTodo}
+      />
 
-   //Data component
-//Footer
+    </div>
 
   
-    </>
-  )
+  );
 }
 
 export default App
---
+++ TodoApp/todo-app/src/components/TodoManager/Input/Input.tsx
@@ -0,0 +1,4 @@
+export const inputForm = (props)=>{
+
+    return (<div><input placeholder="Enter a task"></input> <button>Add</button></div>)
+}
