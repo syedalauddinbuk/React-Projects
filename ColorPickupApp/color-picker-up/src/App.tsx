d8302e39b02ec1cfcf449277906a006d5a14295f
Syed Alauddin Bukhari <syedalauddin.b@gmail.com>
2026-02-19 22:16:36 -0700

    feat: implement color picker functionality and remove unused styles

--- ColorPickupApp/color-picker-up/src/App.tsx
+++ ColorPickupApp/color-picker-up/src/App.tsx
@@ -2,32 +2,21 @@ import { useState } from 'react'
 import reactLogo from './assets/react.svg'
 import viteLogo from '/vite.svg'
 import './App.css'
+import ColorpickerInput from './component/ColorPickerInput'
 
 function App() {
-  const [count, setCount] = useState(0)
+  const [color, setColor] = useState("")
+
+
+  function handleColorChange(event:React.ChangeEvent<HTMLInputElement>) {
+    setColor(event.target.value)
+  }
 
   return (
     <>
-      <div>
-        <a href="https://vite.dev" target="_blank">
-          <img src={viteLogo} className="logo" alt="Vite logo" />
-        </a>
-        <a href="https://react.dev" target="_blank">
-          <img src={reactLogo} className="logo react" alt="React logo" />
-        </a>
-      </div>
-      <h1>Vite + React</h1>
-      <div className="card">
-        <button onClick={() => setCount((count) => count + 1)}>
-          count is {count}
-        </button>
-        <p>
-          Edit <code>src/App.tsx</code> and save to test HMR
-        </p>
-      </div>
-      <p className="read-the-docs">
-        Click on the Vite and React logos to learn more
-      </p>
+      <h1>Color Picker App</h1>
+      < ColorpickerInput handleColorChange={handleColorChange} value={color} />
+      <div style={{ backgroundColor: color, width: "100px", height: "100px" }}></div>
     </>
   )
 }
