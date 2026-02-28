db26bc7fe29e430067ce55ac57d9918c3194ea07
Syed Alauddin Bukhari <syedalauddin.b@gmail.com>
2026-02-27 21:57:34 -0700

    feat: update dependencies and implement quote fetching functionality

---
+++ Random Quote Generator/random-quote-generator/src/component/DisplayQuote.tsx
@@ -0,0 +1,43 @@
+import {useState} from 'react';
+import {fetchQuotes} from '../services/quoteServices';
+
+function DisplayQuotes(){
+
+    const [quote, setQuote] = useState<string>('');
+    const [loading, setLoading] = useState<boolean>(false);
+    const [error, setError] = useState<string>('');
+
+
+    const  getQuote= async() => {
+
+        try{
+        setLoading(true);
+        setError('');
+        const data = await fetchQuotes();
+        setQuote(data);
+
+
+        }catch(error){
+            setError('Failed to fetch quote');
+        } finally{
+            setLoading(false);
+        }
+    }
+
+
+
+
+
+    return (<div>
+       {loading && <p>Loading...</p>}
+       {error&& <p>{error} </p>}
+       {quote&&(
+        <>
+<h2>{quote.content}</h2>
+<p>{quote.author}</p>
+       </>)}
+       
+<button onClick={getQuote}>Get Quote</button>
+       
+    </div>)
+}
--- Random Quote Generator/random-quote-generator/src/services/quoteServices.tsx
+++ Random Quote Generator/random-quote-generator/src/services/quoteServices.tsx
@@ -3,7 +3,11 @@ import type { Quote } from "../types/quotes";
 
 async function fetchQuotes(): Promise<Quote[]>{
 
-    const response = await fetch(",")
+    const response = await fetch("https://api.api-ninjas.com/v2/quotes?categories=success%2Cwisdom", {
+        headers:{
+            "X-Api-Key": import.meta.env.VITE_API_KEY
+        }
+    })
 
     if(!response.ok){
         throw new Error("Failed to fetch quotes")
@@ -16,4 +20,6 @@ async function fetchQuotes(): Promise<Quote[]>{
 
 
 }
+
+export default fetchQuotes
 
